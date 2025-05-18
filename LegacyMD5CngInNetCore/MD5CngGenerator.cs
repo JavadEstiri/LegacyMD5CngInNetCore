55ef9a96494538c0a70a34ee60e33ce3f8e13dea
Iman Estiri <javadestiri344@gmail.com>
2025-05-19 02:48:46 +0330

    Add project files.

---
+++ LegacyMD5CngInNetCore/MD5CngGenerator.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography;
+using System.Text;
+namespace LegacyMD5CngInNetCore
+{
+    public class MD5CngGenerator
+    {
+        /// <summary>
+        ///     The MD5Cng class (part of the .NET Framework's System.Security.Cryptography namespace) 
+        ///     was a CNG (Cryptography Next Generation)-based implementation of the MD5 hashing algorithm. 
+        ///     However, it has been deprecated and discouraged in favor of more secure alternatives.
+        ///     The MD5Cng class remains in older .NET Framework versions .
+        ///     This method Gives You Same Output as MD5Cng Using "System.Security.Cryptography.CngAlgorithm.MD5" Which
+        ///     Is Available in .NET Core and .NET 5+ .
+        /// </summary>
+        /// <param name="input"></param>
+        /// <returns>Hashed String Same As Legacy 'MD5Cng'</returns>
+        public string HashWithCngAlgorithm(string input)
+        {
+            var hashObject = HashAlgorithm.Create(CngAlgorithm.MD5.ToString());
+
+            byte[] inputBytes = Encoding.Unicode.GetBytes(input);
+
+            byte[] hashBytes = hashObject.ComputeHash(inputBytes);
+
+            string hash = Encoding.Default.GetString(hashBytes);
+
+            return hash;
+        }
+    }
+}
---
+++ LegacyMD5CngInNetCore/Program.cs
@@ -0,0 +1,19 @@
+﻿namespace LegacyMD5CngInNetCore
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("\n\n\tEnter Any Text : ");
+            string input = Console.ReadLine();
+
+            var _mD5CngGenerator = new MD5CngGenerator();
+            Console.WriteLine($"\n\tMD5Cng Result  : {_mD5CngGenerator.HashWithCngAlgorithm(input)}");
+            Console.WriteLine($"\n\n\tThere Might Be Some '?' In Returned Hash . " +
+                $"Its Because Im Using 'Encoding.Unicode'." +
+                $"\n\tChange Encoding And Decoding As You Used To"
+            );
+            Console.ReadKey();
+        }
+    }
+}
